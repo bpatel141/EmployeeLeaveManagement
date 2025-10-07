5768644742f6fc44ace05d7ef33016d447731f86
Bhargav <bhargav2242@gmail.com>
2025-10-07 14:41:20 +0530

    Fix Cors

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,15 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        hmr: {
+            host: 'laravel.local',
+        },
+        cors: {
+            origin: ['http://laravel.local', 'http://localhost', 'http://127.0.0.1'],
+            credentials: true,
+        },
+    },
 });
